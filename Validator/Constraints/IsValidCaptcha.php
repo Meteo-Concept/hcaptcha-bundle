bea51b74ee26334f130e8268bf84ef0756b2dd3c
danielesteve-cibervoluntarios <101350646+danielesteve-cibervoluntarios@users.noreply.github.com>
2022-08-02 11:58:59 +0200

    Adding support for PHP 8.0 Attributes

--- Validator/Constraints/IsValidCaptcha.php
+++ Validator/Constraints/IsValidCaptcha.php
@@ -7,7 +7,9 @@ use Symfony\Component\Validator\Constraint;
 /**
  * @brief Screams when the user has failed to solve the CAPTCHA
  * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 class IsValidCaptcha extends Constraint
 {
     /**
