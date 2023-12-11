38f4032776d4e3936dc8571d1973904813aa42b5
Laurent Georget <laurent.georget@meteo-concept.fr>
2023-12-11 15:26:35 +0100

    Fix the loading of split configuration files in function tests

--- Tests/Functional/AppKernel.php
+++ Tests/Functional/AppKernel.php
@@ -24,9 +24,9 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/test_config_functional.yml');
         if (\Symfony\Component\HttpKernel\Kernel::MAJOR_VERSION > 5) {
-            $kernel->addConfigFile(__DIR__.'/test_framework_config_sf6.yml');
+            $loader->load(__DIR__.'/test_framework_config_sf6.yml');
         } else {
-            $kernel->addConfigFile(__DIR__.'/test_framework_config.yml');
+            $loader->load(__DIR__.'/test_framework_config.yml');
         }
     }
 }
