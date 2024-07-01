98f308e26ad508c962953041a09d03e347b3368a
Laurent Georget <laurent.georget@meteo-concept.fr>
2024-07-01 17:33:27 +0200

    Fix a DependencyInjection deprecation

--- DependencyInjection/MeteoConceptHCaptchaExtension.php
+++ DependencyInjection/MeteoConceptHCaptchaExtension.php
@@ -3,7 +3,7 @@
 namespace MeteoConcept\HCaptchaBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
